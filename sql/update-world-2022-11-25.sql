0fa606538a23af29b4f3865dfc820b9f3987e79c
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2022-11-25 18:23:11 -0300

    Create update-world-2022-11-25.sql
    Adicionando montarias de carona ao vendedor de herança

---
+++ sql/update-world-2022-11-25.sql
@@ -0,0 +1,3 @@
+/*Adicionando montarias de carona ao vendedor de heranca*/
+insert into npc_vendor(entry, slot, item, maxcount, incrtime, ExtendedCost, type) values (90000, 0, 54860, 0, 0, 0, 1);
+insert into npc_vendor(entry, slot, item, maxcount, incrtime, ExtendedCost, type) values (90000, 0, 83086, 0, 0, 0, 1);
