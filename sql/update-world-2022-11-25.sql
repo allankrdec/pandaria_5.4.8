d67fa2e0676b450a2b1bfa2675c0461619f46b7b
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2022-11-25 21:48:39 -0300

    Update update-world-2022-11-25.sql

--- sql/update-world-2022-11-25.sql
+++ sql/update-world-2022-11-25.sql
@@ -1,3 +1,10 @@
 /*Adicionando montarias de carona ao vendedor de heranca*/
 insert into npc_vendor(entry, slot, item, maxcount, incrtime, ExtendedCost, type) values (90000, 0, 54860, 0, 0, 0, 1);
 insert into npc_vendor(entry, slot, item, maxcount, incrtime, ExtendedCost, type) values (90000, 0, 83086, 0, 0, 0, 1);
+
+
+/*Adicionando moedas como bravura dominacao, justica e honra ao vendedor comandante-lo-ping em pandaria */
+insert into npc_vendor(entry, slot, item, maxcount, incrtime, ExtendedCost, type) values (64607, 0, 110005, 0, 0, 0, 1);
+insert into npc_vendor(entry, slot, item, maxcount, incrtime, ExtendedCost, type) values (64607, 0, 110006, 0, 0, 0, 1);
+insert into npc_vendor(entry, slot, item, maxcount, incrtime, ExtendedCost, type) values (64607, 0, 110007, 0, 0, 0, 1);
+insert into npc_vendor(entry, slot, item, maxcount, incrtime, ExtendedCost, type) values (64607, 0, 110008, 0, 0, 0, 1);
