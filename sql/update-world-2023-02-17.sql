c8024270af5465efd28b4fb4344aa56eda60207b
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2023-02-17 20:58:33 -0300

    Create update-world-2023-02-17.sql
    Ajustando quests para pegar capa lendaria. Já no inicio consegue pegar sem fazer todas as quests

---
+++ sql/update-world-2023-02-17.sql
@@ -0,0 +1,26 @@
+delete from creature_queststarter where id = 69782 and quest = 32861;
+insert into creature_queststarter (id, quest) values (69782, 32861);
+
+delete from creature_queststarter where id = 69782 and quest = 33104;
+insert into creature_queststarter (id, quest) values (69782, 33104);
+
+delete from creature_questender where id = 69782 and quest = 32861;
+insert into creature_questender (id, quest) values (69782, 32861);
+
+delete from creature_questender where id = 69782 and quest = 33104;
+insert into creature_questender (id, quest) values (69782, 33104);
+
+delete from creature_queststarter where id = 71424 and quest = 33104;
+insert into creature_queststarter (id, quest) values (71424, 33104);
+
+delete from creature_queststarter where id = 64616 and quest = 33104;
+insert into creature_queststarter (id, quest) values (64616, 33104);
+
+delete from creature_queststarter where id = 64616 and quest = 33104;
+insert into creature_queststarter (id, quest) values (64616, 33104);
+
+delete from creature_questender where id = 64616 and quest = 32861;
+insert into creature_questender (id, quest) values (64616, 32861);
+
+delete from creature_questender where id = 64616 and quest = 33104;
+insert into creature_questender (id, quest) values (64616, 33104);
