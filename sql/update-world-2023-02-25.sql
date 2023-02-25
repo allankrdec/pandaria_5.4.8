4c861907150fec328f08aa807de65ee2637896e8
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2023-02-25 13:17:59 -0300

    Create update-world-2023-02-25.sql
    /*Adicionando armas de herança de nivel 582*/

---
+++ sql/update-world-2023-02-25.sql
@@ -0,0 +1,8 @@
+/*Adicionando armas de herança de nivel 582*/
+
+delete from npc_vendor where entry=90000 and item in (select entry from item_template where itemlevel = 582
+and name like 'Hellscream%');
+
+insert into npc_vendor
+(select 90000 as entry, 0 as slot, entry as item, 0 as maxcount, 0 as incrtime, 0 as extendedcost, 1 as type from item_template where itemlevel = 582
+and name like 'Hellscream%');
