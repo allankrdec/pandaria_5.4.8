665b23f47416c0ea245fdd453592f6edd251950d
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2023-04-07 07:33:55 -0300

    Update update-world-2023-04-02.sql

--- sql/update-world-2023-04-02.sql
+++ sql/update-world-2023-04-02.sql
@@ -10,3 +10,7 @@ insert into creature (guid, id, map, zoneId, areaId, spawnMask, phaseMask, model
 //adicionando loot para terras do fogo 10Heroic, item 69224
 delete from creature_loot_template where entry=52409 and item = 69224 and lootmode='RAID_10MAN_HEROIC';
 insert into creature_loot_template (entry, item, ChanceOrQuestChance, lootmode, groupid, mincountOrRef, maxcount) values (52409, 69224, 100.0, 'RAID_10MAN_HEROIC', 0, 1, 1);
+
+//adicionando loot para terras do fogo 10Heroic, item 71665
+delete from creature_loot_template where entry=52530 and item = 71665 and lootmode='RAID_10MAN_HEROIC';
+insert into creature_loot_template (entry, item, ChanceOrQuestChance, lootmode, groupid, mincountOrRef, maxcount) values (52530, 71665, 100.0, 'RAID_10MAN_HEROIC', 0, 1, 1);
