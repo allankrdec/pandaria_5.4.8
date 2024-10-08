4303c50a5ed1b26bb05ac8235047d46e95b5ad28
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-10-08 11:05:34 -0300

    Create update-world-2024-10-08.sql

---
+++ sql/update-world-2024-10-08.sql
@@ -0,0 +1,2 @@
+/*Correção do portal para Geodomo*/
+update spell_target_position  set target_map=646, target_position_x = 909.809, target_position_y=502.65  ,target_position_z= -49.2188, target_orientation=0.158003 where id = 84464;
