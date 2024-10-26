5473e23c3479f8e4df75142b0101c8fdef56b757
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-10-26 13:32:54 -0300

    Update update-world-2024-10-26.sql

--- sql/update-world-2024-10-26.sql
+++ sql/update-world-2024-10-26.sql
@@ -1,3 +1,6 @@
 /*problema na quest Cortando os Ceus*/
 
 update quest_template set flags=65536  where id=31810;
+
+/*ajustando chance de drop de itens*/
+update creature_loot_template set chanceorquestchance=100 where entry=73666;
