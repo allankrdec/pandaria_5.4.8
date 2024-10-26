c29927fb08c721af7b1ae2fdd765b7238bf86098
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-10-26 13:44:02 -0300

    Update update-world-2024-10-26.sql

--- sql/update-world-2024-10-26.sql
+++ sql/update-world-2024-10-26.sql
@@ -3,4 +3,5 @@
 update quest_template set flags=65536  where id=31810;
 
 /*ajustando chance de drop de itens*/
-update creature_loot_template set chanceorquestchance=100 where entry=73666;
+update creature_loot_template set chanceorquestchance=30 where entry=73666 and item <> 86574;
+update creature_loot_template set chanceorquestchance=100, maxcount=10 where entry=73666 and item = 86574;
