7b730adceed68d536979d052fbdcea51b79d2572
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-10-31 13:26:49 -0300

    Update update-world-2024-10-30.sql

--- sql/update-world-2024-10-30.sql
+++ sql/update-world-2024-10-30.sql
@@ -7,3 +7,7 @@ update item_template set RequiredLevel=99, requiredReputationFaction= 1359, Requ
 
 /*Adicionando 100% de chance para o item 24361 - Espada Longa do Fogo Místico*/
 update creature_loot_template set ChanceOrQuestChance=100 where item=24361;
+
+/*Adicionando item ao loot do boss 17941*/
+delete from creature_loot_template where entry=17941 and item=31336;
+insert into creature_loot_template(entry, item, ChanceOrQuestChance, lootmode, groupid, mincountOrRef, maxcount) value(17941, 31336, 100, 'DUNGEON_NORMAL', 1, 1, 1) ;
