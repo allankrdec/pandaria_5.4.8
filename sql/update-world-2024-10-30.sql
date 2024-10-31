1406950312acb718035f8a8525ef3506faa628dc
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-10-31 13:15:36 -0300

    Update update-world-2024-10-30.sql

--- sql/update-world-2024-10-30.sql
+++ sql/update-world-2024-10-30.sql
@@ -1,2 +1,6 @@
 /*Correção do item da capa lendaria*/
-update item_template set RequiredLevel=99, requiredReputationFaction= 1359, RequiredReputationRank=4 where entry=102457;
+update item_template set requiredreputationrank=5 where entry=110021;
+
+/*Adicionando reputacao mínima aos itens de subir de level*/
+update item_template set requiredReputationFaction=1435, requiredreputationrank=4 where entry=110020;
+update item_template set RequiredLevel=99, requiredReputationFaction= 1359, RequiredReputationRank=5 where entry=102457;
