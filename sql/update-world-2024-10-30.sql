838fd40604743f8585fcb8dc5f56d1626e6bdfb6
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-10-31 13:20:03 -0300

    Update update-world-2024-10-30.sql

--- sql/update-world-2024-10-30.sql
+++ sql/update-world-2024-10-30.sql
@@ -4,3 +4,6 @@ update item_template set requiredreputationrank=5 where entry=110021;
 /*Adicionando reputacao mínima aos itens de subir de level*/
 update item_template set requiredReputationFaction=1435, requiredreputationrank=4 where entry=110020;
 update item_template set RequiredLevel=99, requiredReputationFaction= 1359, RequiredReputationRank=5 where entry=102457;
+
+/*Adicionando 100% de chance para o item 24361 - Espada Longa do Fogo Místico*/
+update creature_loot_template set ChanceOrQuestChance=100 where item=24361;
