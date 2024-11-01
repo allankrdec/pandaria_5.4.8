39d736c89219560c1dc9754c5b2d68fa207a4e2d
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-11-01 19:42:40 -0300

    Update update-world-2024-11-01.sql

--- sql/update-world-2024-11-01.sql
+++ sql/update-world-2024-11-01.sql
@@ -1,3 +1,3 @@
 /*Ajustando loot do cristal da insanidade*/
 update creature_loot_template set ChanceOrQuestChance=100 where entry=50339 and item=86569;
-update gameobject_loot_template set ChanceOrQuestChance=10 where item=86569 and entry=222686;
+update gameobject_loot_template set ChanceOrQuestChance=20 where item=86569 and entry=222686;
