bd70790a68cee95f6bb05885db99c13f7272a081
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-11-01 21:37:30 -0300

    Update update-world-2024-11-01.sql

--- sql/update-world-2024-11-01.sql
+++ sql/update-world-2024-11-01.sql
@@ -2,5 +2,5 @@
 update creature_loot_template set ChanceOrQuestChance=100 where entry=50339 and item=86569;
 update gameobject_loot_template set ChanceOrQuestChance=20 where item=86569 and entry=222686;
 
-/*Ajustando xp para level maior que 97*/
-update player_xp_for_level set xp_for_next_level=27371000 where lvl>=98;
+/*Ajustando xp para level maior que 96*/
+update player_xp_for_level set xp_for_next_level=25371000 where lvl>=95;
