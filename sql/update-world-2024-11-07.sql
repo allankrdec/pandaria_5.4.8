2a60e41adc4aa0066c4d664501cf81b104f9cbdd
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-11-08 22:55:49 -0300

    Update update-world-2024-11-07.sql

--- sql/update-world-2024-11-07.sql
+++ sql/update-world-2024-11-07.sql
@@ -2,3 +2,6 @@
 
 update quest_template set flags=65536 where id=30698;
 update quest_objective set amount=0 where questid=30698;
+
+/*ajustando todas as quest autocomplet*/
+update quest_objective set amount=0 where questid in (select id from quest_template where flags=65536);
