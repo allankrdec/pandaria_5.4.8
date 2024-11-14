a3df94ed0537ba34790cb232a529d1ce941871f5
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-11-13 22:37:44 -0300

    Update update-world-2024-11-13.sql

--- sql/update-world-2024-11-13.sql
+++ sql/update-world-2024-11-13.sql
@@ -1,2 +1,5 @@
 /*Ajustando quest - Paz aos Barbatana de Pérola*/
 delete from quest_objective where questid=29883 and id=252422;
+
+/*Ajustando loot do ordos*/
+update creature_loot_template set chanceorquestchance=20 where entry=72057;
