ce65995da7367fc7b87bd68f0633ee4d109b98b2
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-11-21 15:11:43 -0300

    Update update-world-2024-11-21.sql

--- sql/update-world-2024-11-21.sql
+++ sql/update-world-2024-11-21.sql
@@ -9,3 +9,9 @@ update creature_loot_template set chanceorquestchance=100 where entry=72057 and 
 delete from item_loot_template where entry=104273 and item in(select item from creature_loot_template where entry=72057);
 insert into item_loot_template(entry, item, ChanceOrQuestChance, lootmode, groupid, mincountorref, maxcount)
 select 104273 as entry, item, 20 as ChanceOrQuestChance, lootmode, groupid, mincountorref, maxcount from creature_loot_template where entry=72057;
+
+
+/*Ajustando creatures da ilha perene*/
+update creature set zoneid=6757, phaseMask=65535 where id=300001;
+update creature set zoneid=6757, phaseMask=65535 where id=300002;
+update creature_template set Health_mod=144*1.3 where entry=300002;
