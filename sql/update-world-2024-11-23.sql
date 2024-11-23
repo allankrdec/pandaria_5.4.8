e54437e462753f83794092d18718c0b06f9ff4bb
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-11-23 17:53:40 -0300

    Update update-world-2024-11-23.sql

--- sql/update-world-2024-11-23.sql
+++ sql/update-world-2024-11-23.sql
@@ -4,3 +4,4 @@ update item_template set class=0, subclass=0 where entry in (select item from np
 /*ajustando loot do ordos*/
 delete from creature_loot_template where entry=72057 and item <> 104273;
 delete from item_loot_template where entry=104273 and item=104273
+delete from personal_loot_item where entry=7205799;
