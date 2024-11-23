1891a66be521be2f3e08fa8fe2450e6fd91b7384
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-11-23 13:52:03 -0300

    Update update-world-2024-11-23.sql

--- sql/update-world-2024-11-23.sql
+++ sql/update-world-2024-11-23.sql
@@ -1,2 +1,5 @@
 /*Ajustando flags da capa lendaria*/
 update item_template set class=0, subclass=0 where entry in (select item from npc_vendor where entry=70436 and class=4 and subclass=1);
+
+/*ajustando loot do ordos*/
+delete from creature_loot_template where entry=72057 and item <> 104273;
