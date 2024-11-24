b93f7616e2d6bc601b2beea7313a1d9231aac59a
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-11-24 11:05:20 -0300

    Update update-world-2024-11-23.sql

--- sql/update-world-2024-11-23.sql
+++ sql/update-world-2024-11-23.sql
@@ -4,4 +4,8 @@ update item_template set class=0, subclass=0 where entry in (select item from np
 /*ajustando loot do ordos*/
 delete from creature_loot_template where entry=72057 and item <> 104273;
 delete from item_loot_template where entry=104273 and item=104273
-delete from personal_loot_item where entry=7205799;
+
+delete from personal_loot_item where entry=72057;
+insert personal_loot_item (entry, item) values(72057, 104273);
+update creature_loot_template set chanceorquestchance=0 where item=104273;
+update bonus_loot_template set entry=7205799 where entry=72057;
