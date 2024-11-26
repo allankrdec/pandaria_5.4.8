792220407539e17da4dc981232c2daa28a057a7e
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-11-26 18:35:02 -0300

    Update update-world-2024-11-26.sql

--- sql/update-world-2024-11-26.sql
+++ sql/update-world-2024-11-26.sql
@@ -2,3 +2,6 @@
 
 update quest_template set flags=65536 where id=29408;
 update quest_objective set amount=0 where questid=29408;
+
+update quest_template set flags=65536 where id=29774;
+update quest_objective set amount=0 where questid=29774;
