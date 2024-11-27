05d6d17b703955be20647b730a3983bcaba79502
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-11-26 22:20:16 -0300

    Update update-world-2024-11-26.sql

--- sql/update-world-2024-11-26.sql
+++ sql/update-world-2024-11-26.sql
@@ -8,3 +8,11 @@ update quest_objective set amount=0 where questid=29774;
 
 update quest_template set flags=65536 where id=29792;
 update quest_objective set amount=0 where questid=29792;
+
+update quest_template set flags=65536 where id=29799;
+update quest_objective set amount=0 where questid=29799;
+
+update quest_template set flags=65536 where id in (select quest from creature_queststarter where id=66260) and flags=2097152;
+update quest_objective set amount=0 where questid in (select quest from creature_queststarter where id=66260) and flags=2097152;
+
+/*Verifica depois a quest final. Um novo Destino*/
