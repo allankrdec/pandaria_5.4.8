70d6567d3dca332e5e0138dd9660f64e6bc0af10
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-11-28 18:53:11 -0300

    Update update-world-2024-11-28.sql

--- sql/update-world-2024-11-28.sql
+++ sql/update-world-2024-11-28.sql
@@ -1,3 +1,5 @@
 /*Ajustando loot do ordos, apos configurar servidor para loot ser diário*/
-
 update item_loot_template set ChanceOrQuestChance=8 where entry=104273 and ChanceOrQuestChance=12;
+
+/*Alterando respaw do ordos para 15min*/
+update creature set spawntimesecs=900 where id=72057;
