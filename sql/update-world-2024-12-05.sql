34bbed0307db706c3d4511d495c3bd733b18b609
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-12-05 20:13:47 -0300

    Update update-world-2024-12-05.sql

--- sql/update-world-2024-12-05.sql
+++ sql/update-world-2024-12-05.sql
@@ -1,4 +1,4 @@
 /*Loot da raide certo a orgrimar*/
 
-update creature_loot_template set chanceorquestChance=30, mincountorRef=1
+update creature_loot_template set chanceorquestChance=100, mincountorRef=1
 where entry in (71543, 71475, 71479, 71480, 72249, 71466, 71859, 71515, 71454, 71889, 71529, 71152) and item > 100000;
