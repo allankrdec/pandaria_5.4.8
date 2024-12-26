155e251fdbdab27548e90ce67e323e7c427bde8c
Allan Kardec <52975875+allankrdec@users.noreply.github.com>
2024-12-26 06:57:15 -0300

    Update update-world-2024-12-03.sql

--- sql/update-world-2024-12-03.sql
+++ sql/update-world-2024-12-03.sql
@@ -5,6 +5,11 @@ update item_loot_template set ChanceOrQuestChance=15 where entry=104273 and Chan
 update creature_loot_template set ChanceOrQuestChance = ChanceOrQuestChance * 2 where entry=71865 and ChanceOrQuestChance in (7.5, 14.5, 18);
 
 /*Adicionando cx de correio na ilha perene*/
-delete from gameobject where guid=549936;
+delete from gameobject where guid=550118;
+delete from gameobject where guid=550026;
+
 insert into gameobject (guid, id, map, zoneId, areaId, spawnMask, phaseMask, phaseId, position_x, position_y, position_z, orientation, rotation0, rotation1, rotation2, rotation3, spawntimesecs, animprogress, state, ScriptName) 
-values (549936, 444007, 730, 0, 0, 1, 1, 0, 851.331, 1061.94, -9.13624, 4.483, -0.0, -0.0, -0.783385, 0.621536, 300, 0, 1, '');
+values (550118, 32349, 870, 0, 0, 1, 1, 0, -51.6538, -5359.92, 150.11, 1.43068, -0.0, -0.0, -0.655874, -0.754871, 300, 0, 1, '');
+
+insert into gameobject (guid, id, map, zoneId, areaId, spawnMask, phaseMask, phaseId, position_x, position_y, position_z, orientation, rotation0, rotation1, rotation2, rotation3, spawntimesecs, animprogress, state, ScriptName) 
+values (550026, 32349, 870, 0, 0, 1, 1, 0, -788.661, -5063.38, 2.43943, 0.00549722, -0.0, -0.0, -0.00274861, -0.999996, 300, 0, 1, '');
